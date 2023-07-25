10681a6856aad9479d5137d10cb1b38f47b755ae
sherlockian4869 <k.yaeo@carep-tech.com>
2023-07-25 22:34:09 +0900

    リダイレクトURLを設定

--- src/app/connected_line/page.tsx
+++ src/app/connected_line/page.tsx
@@ -3,7 +3,7 @@ import NextLink from 'next/link'
 import { Button, Flex } from '@/common/design'
 
 export default function ConnectedLineScreen() {
-  const url = `https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=${process.env.NEXT_PUBLIC_CLIENT_ID}&redirect_uri=http://127.0.0.1:3000/line&state=hoge&bot_prompt=normal&scope=profile%20openid&nonce=foobar&prompt=consent`
+  const url = `https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=${process.env.NEXT_PUBLIC_CLIENT_ID}&redirect_uri=${process.env.NEXT_PUBLIC_DOMAIN}/line&state=hoge&bot_prompt=normal&scope=profile%20openid&nonce=foobar&prompt=consent`
   return (
     <Flex width='100%' justifyContent='center'>
       <Button
