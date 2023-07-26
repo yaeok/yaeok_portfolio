a20dcaf3b8bd7b403acdcb02d8f46f484424b59f
rev-kohei.yaeo <yaeok@revolveok.co.jp>
2023-07-26 14:18:07 +0900

    少し修正

--- src/app/new_connected_line/page.tsx
+++ src/app/new_connected_line/page.tsx
@@ -7,9 +7,9 @@ import liff from '@line/liff'
 export default function NewConnectedLineScreen() {
   const [userId, setUserId] = useState('')
   const onClickConnectLine = async () => {
-    const userInfo = await liff.getProfile()
-    const id = userInfo.userId
-    setUserId(id)
+    const userInfo = await liff.getContext()
+    const id = userInfo?.userId
+    setUserId(id ?? '取得できませんでした')
   }
   return (
     <Flex flexDirection='column'>
