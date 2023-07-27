765bbe5ddfb1634407bd3990fb602ddb63411e73
rev-kohei.yaeo <yaeok@revolveok.co.jp>
2023-07-27 17:27:50 +0900

    画面の整理

--- src/common/constants/path.ts
+++ src/common/constants/path.ts
@@ -1,3 +1,5 @@
+import { type } from 'os'
+
 /** APIのエンドポイント */
 export const API_PATH = {
   BROADCAST: process.env.NEXT_PUBLIC_DOMAIN + '/api/line/sent_broadcast',
@@ -5,31 +7,50 @@ export const API_PATH = {
 }
 
 /** ページ名・ルート名 */
-export const INDEX = { path: '/', name: 'index' }
-export const AUTH = { path: '/auth', name: 'auth' }
-export const HOME = { path: '/home', name: 'home' }
-export const PORTFOLIO = { path: '/portfolio', name: 'ポートフォリオ' }
+export const INDEX = { path: '/', name: 'ルート' }
+export const AUTH = { path: '/auth', name: 'ログイン画面' }
+export const HOME = { path: '/home', name: 'ホーム画面' }
+/** ポートフォリオ */
+export const PORTFOLIO = {
+  path: '/portfolio',
+  name: 'ポートフォリオ画面',
+  type: '',
+}
+/** LINE関連 */
 export const LINE_BROADCAST = {
   path: '/line_broadcast',
-  name: 'LINE：Broadcast送信',
+  name: 'Broadcast送信',
+  type: 'LINE',
 }
-export const CONNECTED_SPRING_BOOT = {
-  path: '/connected_spring_boot',
-  name: 'Spring-Boot：API連携',
+export const LINE_CONNECT = {
+  path: '/line_login',
+  name: 'ユーザ連携',
+  type: 'LINE',
 }
-export const CUSTOMER_LIST = { path: '/customer', name: 'お客さま一覧' }
-export const LINE_CONNECT = { path: '/line_login', name: 'LINE：ユーザ連携' }
 export const LINE_PUSHMESSAGE = {
   path: '/line_pushmessage',
-  name: 'LINE：メッセージ送信',
+  name: 'メッセージ送信',
+  type: 'LINE',
+}
+/** API関連:Spring-Boot */
+export const SPRING_BOOT_CONNECT = {
+  path: '/spring_boot_connect',
+  name: 'API連携',
+  type: 'Spring-Boot',
+}
+/** Next.js × ChakraUI 標準的な開発 */
+export const CUSTOMER_LIST = {
+  path: '/customer',
+  name: 'お客さま一覧',
+  type: '標準開発',
 }
 
 /** ホーム画面に表示するボタン */
 export const SHOW_HOME = [
   PORTFOLIO,
-  CONNECTED_SPRING_BOOT,
   LINE_BROADCAST,
   LINE_CONNECT,
   LINE_PUSHMESSAGE,
+  SPRING_BOOT_CONNECT,
   CUSTOMER_LIST,
 ]
